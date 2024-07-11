6c44b8f87f4bcd8b0890122629aebc131829dc17
Eduardo <eduardoj.schroeder@gmail.com>
2024-07-11 16:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        if (filme != null)
+        {
+            filmes.Add(filme);
+            Console.WriteLine($"Titulo: {filme.Titulo}, Genero: {filme.Genero}, Duração: {filme.Duracao} minutos");
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres.")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatório.")]
+    [Range(45, 600, ErrorMessage = "A duração do filme deve ter entre 45 e 600 minutos.")]
+    public int Duracao { get; set; }
+    public string Diretor { get; set; }
+}
